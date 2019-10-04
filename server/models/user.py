6b6ce95663a5f05879f392a464b00c13963a9be4
Amar Gill <amar.s.gill@outlook.com>
2019-10-04 20:04:46 +0700

    resolved user model migration issue - import org model required

--- server/models/user.py
+++ server/models/user.py
@@ -1,8 +1,9 @@
 from models.base_model import BaseModel
+from models.organization import Organization
 import peewee as pw
 
 class User(BaseModel):
     username = pw.CharField(unique=True)
     email = pw.CharField(unique=True)
     password = pw.CharField()
-    organization = pw.ForeignKeyField(default=None)
+    organization = pw.ForeignKeyField(Organization, null=True, default=None)
