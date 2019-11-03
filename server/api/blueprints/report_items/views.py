21de39f8fe5a81651bcb6955ec381c389ded21fd
Amar Gill <amar.s.gill@outlook.com>
2019-11-03 16:46:51 +0700

    report_item model created. report_item.create and reports.items API endpoints tested and working.

---
+++ server/api/blueprints/report_items/views.py
@@ -0,0 +1,46 @@
+from flask import Blueprint, jsonify, request
+from models.report import Report
+from models.report_item import ReportItem
+
+
+report_items_api_blueprint = Blueprint("report_items_api",
+                            __name__,
+                            template_folder= "templates")
+
+@report_items_api_blueprint.route("/", methods=["POST"])
+def create():
+    # get data
+    subject = request.json.get("subject", None)
+    content = request.json.get("content", None)
+    report_id = request.json.get("reportId", None)
+
+    # data validation
+    if not report_id:
+        return jsonify(
+            message = "Missing reportId",
+            status = "Fail"
+        )
+
+    # calculate report_item_index
+    report_item_count = ReportItem.select().where(ReportItem.report_id == report_id).count()
+    report_item_index = report_item_count + 1
+
+    # instantiate new_report_item and save to db
+    new_report_item = ReportItem(
+                        subject = subject,
+                        content = content,
+                        report_item_index=report_item_index,
+                        report_id=report_id
+                        )
+
+    if new_report_item.save():
+        return jsonify(
+            message = "New report item created.",
+            status = "Success"
+        )
+    else:
+        return jsonify(
+            message = "Something went wrong please try again",
+            status = "Fail"
+        )
+        
---
+++ server/models/report_item.py
@@ -0,0 +1,11 @@
+from models.base_model import BaseModel
+from models.report import Report
+import datetime
+import peewee as pw
+
+# use inheritance for field / h&s reports?
+class ReportItem(BaseModel):
+    subject = pw.CharField(null=True)
+    content = pw.CharField(null=True)
+    report_item_index = pw.IntegerField(null=True)
+    report = pw.ForeignKeyField(Report, null=False)
