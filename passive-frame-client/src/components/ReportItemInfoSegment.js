683ea1bfbbe97c428d0bc2ae60fcff7460ef90be
Amar Gill <amar.s.gill@outlook.com>
2019-11-05 18:05:34 +0700

    styling commenced for ReportItemInfoSegment.

--- passive-frame-client/src/components/ReportItemInfoSegment.js
+++ passive-frame-client/src/components/ReportItemInfoSegment.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Segment } from 'semantic-ui-react'
+import { Segment, Grid, Menu, Label, Header, Button, Icon } from 'semantic-ui-react'
 
 const ReportItemInfoSegment = props => {
     // set item from props
@@ -7,10 +7,73 @@ const ReportItemInfoSegment = props => {
 
     return (
         <Segment>
-            <h3>{item.id}</h3>
-            <h3>{item.subject}</h3>
-            <h3>{item.content}</h3>
-            <h3>{item.reportItemIndex}</h3>
+            <Grid stackable columns="3">
+                <Grid.Column>
+                    <h3>Item {item.reportItemIndex}</h3>
+                    <h3>Subject: {item.subject}</h3>
+
+                </Grid.Column>
+                <Grid.Column>
+                    <Menu>
+                        <Menu.Item>
+                            <Button
+                            icon
+                            secondary>
+                                <Icon name="edit"/>
+                                Edit Item
+                            </Button>
+                        </Menu.Item>
+                    </Menu>
+                    <Menu>
+                        <Menu.Item>
+                            <Label
+                            color="black"
+                            secondary>
+                                Date Created:
+                                <Label.Detail>
+                                    DATE
+                                </Label.Detail>
+                            </Label>
+                        </Menu.Item>
+                        <Menu.Item>
+                            <Label
+                            color="black"
+                            secondary>
+                                Last Edit:
+                                <Label.Detail>
+                                    DATE
+                                </Label.Detail>
+                            </Label>
+                        </Menu.Item>
+                    </Menu>
+                </Grid.Column>
+                <Grid.Column>
+                    <Header
+                        as="h3"
+                        content="Actions"
+                        subheader="Count: 11"
+                    />
+                    <Menu secondary compact>
+                        <Menu.Item>
+                            <Label color="black" >
+                                Open
+                                    <Label.Detail>5</Label.Detail>
+                            </Label>
+                        </Menu.Item>
+                        <Menu.Item>
+                            <Label color="black" >
+                                Overdue
+                                    <Label.Detail>2</Label.Detail>
+                            </Label>
+                        </Menu.Item>
+                        <Menu.Item>
+                            <Button secondary basic circular icon>
+                                <Icon name='plus' />
+                            </Button>
+                        </Menu.Item>
+                    </Menu>
+                </Grid.Column>
+            </Grid>
         </Segment>
     )
 }
--- passive-frame-client/src/pages/ReportItemsPage.js
+++ passive-frame-client/src/pages/ReportItemsPage.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from 'react'
-import { useParams, useHistory } from 'react-router-dom'
-import { Grid } from 'semantic-ui-react'
+import { useParams, useHistory, Link } from 'react-router-dom'
+import { Grid, Menu, Button, Icon, Input, Header } from 'semantic-ui-react'
 import ReportItemInfoSegment from '../components/ReportItemInfoSegment'
+import StickyHorizontalDivider from '../components/StickyHorizontalDivider'
 
 const ReportItemsPage = (props) => {
     //use states
@@ -42,6 +43,56 @@ const ReportItemsPage = (props) => {
 
     return (
         <div className="mt-42">
+            <Menu className="fixed-submenu bg-white" secondary stackable>
+                <Menu.Item fitted="vertically">
+                    <Button
+                        onClick={() => {
+                            alert("GO BACK")
+                        }}
+                        compact
+                        className="remove-border-radius"
+                        icon
+                        secondary>
+                        <Icon name="chevron left" />
+                    </Button>
+                    <Header
+                        as="h3"
+                        style={{ paddingLeft: 6, marginTop: "auto", marginBottom: "auto" }}
+                        content="CURRONT PROJECT NAME"
+                        subheader="PROJECT NUMBER" />
+                </Menu.Item>
+                <Menu.Menu position="right">
+                    <Menu.Item>
+                        <Button as={Link}
+                            // to={{
+                            //     pathname: "/projects/" + currentProject.id + "/new_report/",
+                            //     state: currentProject
+                            // }}
+                            className="remove-border-radius" secondary basic icon>
+                            <Icon name="file pdf outline" />
+                            Download PDF
+                    </Button>
+
+                    </Menu.Item>
+                    
+
+                    <Menu.Item>
+                        <Button as={Link}
+                            // to={{
+                            //     pathname: "/projects/" + currentProject.id + "/new_report/",
+                            //     state: currentProject
+                            // }}
+                            className="remove-border-radius" secondary icon>
+                            <Icon name="plus" />
+                            New Item
+                    </Button>
+
+                    </Menu.Item>
+
+                </Menu.Menu>
+            </Menu>
+            <StickyHorizontalDivider topDesktop={88} topMobile={171} />
+
             <Grid padded="horizontally" columns="1">
 
                 {
