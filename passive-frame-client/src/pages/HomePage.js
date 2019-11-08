afca35c68b41ad82ea7e99a78ffa7b45729b0ebc
Amar Gill <amar.s.gill@outlook.com>
2019-11-08 14:43:54 +0700

    added responsive behaviour to ZER0_system landing page header.

--- passive-frame-client/src/pages/HomePage.js
+++ passive-frame-client/src/pages/HomePage.js
@@ -1,13 +1,17 @@
 import React from 'react'
-
+import {Responsive} from 'semantic-ui-react'
 const HomePage = () => {
     // set states
 
     return (
         <div className="mt-42">
-            <div style={{ position: "fixed", top: "55vh", left: "25vw", color: "white", backgroundColor: "black" }}>
-                <h1 style={{fontFamily: "Courier New"}} >  [  ZER0_system under development  ]  </h1>
-            </div>
+            <Responsive minWidth={790}>
+                <h1 style={{ position: "fixed", top: "55vh", left: "25vw", color: "white", backgroundColor: "black", fontFamily: "Courier New" }} >  [  ZER0_system under development  ]  </h1>
+            </Responsive>
+            <Responsive maxWidth={789}>
+                <h4 style={{ position: "fixed", top: "95vh", left: "25vw", color: "white", backgroundColor: "black", fontFamily: "Courier New" }} >  [  ZER0_system under development  ]  </h4>
+            </Responsive>
+
             <img src="Epyon-katoki1.jpg" style={{ width: "100vw" }} />
             <img src="009.jpg" style={{ width: "100vw" }} />
             <img src="Epyon-defense.jpg" style={{ width: "100vw" }} />
