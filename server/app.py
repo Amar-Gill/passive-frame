5cf75c09113f364a0c61e095b1ed34a90e8b3008
Amar Gill <amar.s.gill@outlook.com>
2020-01-02 19:10:21 +0700

    notes on deployment and cloud storage.

--- server/app.py
+++ server/app.py
@@ -2,7 +2,6 @@ from flask import Flask, jsonify
 from playhouse.postgres_ext import PostgresqlExtDatabase
 from flask_wtf.csrf import CSRFProtect
 from flask_jwt_extended import JWTManager
-import peewee as pw
 
 app = Flask("passive-frame")
 
